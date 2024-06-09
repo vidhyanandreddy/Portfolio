08d237b21dcbd6cf3d0627a75f5c9c949d0e8101
MULINTI VIDHYANAND REDDY <133974501+vidhyanandreddy@users.noreply.github.com>
2024-06-09 17:56:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+let containerss = document.querySelector(".containerss");
+let timeNow = new Date().getHours();
+// timeNow=10;
+console.log(timeNow);
+let greeting =
+  timeNow >= 5 && timeNow < 12
+    ? "Good Morning"
+    : timeNow >= 12 && timeNow < 18
+    ? "Good Afternoon"
+    : "Good Evening";
+console.log(greeting);
+containerss.innerHTML = `<h2>${greeting}</h2>`;
+
+// typing-text
+var typed = new Typed('.auto-type',{
+    strings : ['Programmer','Designer','Frontend-Developer'],
+    typeSpeed : 150,
+    backSpeed : 150,
+    looped : true,
+});
+
+
